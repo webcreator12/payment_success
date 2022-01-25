7eedf577c337bc8921a6796c4fff708d77cd55ba
web creator <webcreator123456@gmail.com>
2022-01-25 01:17:52 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function viewPassword(){
+    var password = document.getElementById("password");
+    var changeIcon = document.getElementById("changeicon");
+
+    if(password.type == "password"){
+        password.type = "text";
+        changeIcon.className = "fas fa-low-vision";
+    }
+    else{
+        password.type = "password";
+        changeIcon.className = "fas fa-eye";
+    }
+}
+
+var acc = document.getElementsByClassName("accordin");
+var i;
+
+for(i = 0; i<= acc.length;i++){
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        }
+        else{
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
+}
